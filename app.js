bee01a3712b58ce5cded9735ddf43a407ee63531
Choiseokryeol <zlshsktl@naver.com>
2022-06-07 09:58:40 +0900

    .

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'views')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+const express = require('express');
+const path = require('path');
+const index = require('./routes/index');
+const app = express(); 
+app.use('/', index);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
