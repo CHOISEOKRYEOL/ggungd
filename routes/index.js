2b73ab24bb7ca6a273919dd691486ce140d232d4
Choiseokryeol <zlshsktl@naver.com>
2022-06-07 13:47:01 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  fs.readFile('/home/hosting_users/getwis88/apps/ggungd/public/js/example.json', (err, data) => {
-    if(err) throw err;
-    const results = JSON.parse(data.toString('utf8'));
-    res.render('index', { title:"WEB Designer", results });
-  });
+  res.render('index');
 });
 
 module.exports = router;
