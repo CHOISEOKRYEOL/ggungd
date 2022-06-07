09b49f8eb9583256e4b21066b7295a7e6575aa7f
Choiseokryeol <zlshsktl@naver.com>
2022-06-07 13:56:19 +0900

    .

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 8001;
-app.set('views', __dirname + '/views');
-app.set('view engine', 'index.html');
+app.set('views', __dirname + './views');
+app.set('view engine', 'html');
 app.get('/', (req, res) => {
-  res.render('index.html');
-})
+  res.render('index');
+});
 app.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`);
-})
+});
