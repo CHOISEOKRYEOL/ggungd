bcddd1d8307e8db85e9f6398fab33f234ba19929
Choiseokryeol <zlshsktl@naver.com>
2022-06-07 11:58:18 +0900

    .

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
-const PORT = 8001
+const express = require('express');
+const app = express();
+const PORT = 8001;
 app.set('views', __dirname + '/views');
 const a = app.set('view engine', 'ejs');
 console.log(a);
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index');
 })
 app.listen(PORT, () => {
-    console.log(`server started on PORT ${PORT}`)
+    console.log(`server started on PORT ${PORT}`);
 })
