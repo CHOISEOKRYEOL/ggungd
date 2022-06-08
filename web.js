addeab006e5ec35afbcef2b7ebcec4b462b742be
Choiseokryeol <zlshsktl@naver.com>
2022-06-08 10:01:12 +0900

    .

--- web.js
+++ web.js
@@ -5,8 +5,12 @@ const PORT = 8001;
 app.set('views', __dirname + '/views');
 app.set('public', __dirname + '/public');
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/', (req, res) => {
+  res.render('images');
 });
 app.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`);
